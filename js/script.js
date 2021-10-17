fdfa0b242b8a3301933728f8ecc008f2183c55a2
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2021-10-17 14:52:32 -0400

    added partial JS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
 const hamBurger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-
 let mobileMenu = () => {
   hamBurger.classList.toggle("active");
   navMenu.classList.toggle("active");
 };
 
 hamBurger.addEventListener("click", mobileMenu);
+
+let modal = document.querySelector(".modal")
+let modalImg = document.querySelector('.modal-img')
+let img = document.querySelector('.services-img')
+let captionText = document.querySelector(".caption");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+let closeBtn = document.querySelector(".close-btn")
+closeBtn.onclick = function () {
+  modal.style.display="none"
+}
+
