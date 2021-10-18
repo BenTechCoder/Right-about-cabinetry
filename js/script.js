bbb37d937e96bfbdb7774811848b8c0c70e71eb4
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2021-10-18 16:05:14 -0400

    Added custom JS modals and bathroom page

--- js/script.js
+++ js/script.js
@@ -7,24 +7,25 @@ let mobileMenu = () => {
 
 hamBurger.addEventListener("click", mobileMenu);
 
-let modal = document.querySelector(".modal")
-let modalImg = document.querySelector('.modal-img')
-let img = document.querySelector('.services-img')
+let modal = document.querySelector(".modal");
+let modalImg = document.querySelector(".modal-img");
+let img = document.querySelector(".services-img");
 let captionText = document.querySelector(".caption");
+Array.from(document.querySelectorAll(".services-img")).forEach((item) => {
+  item.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  };
+});
 
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
 let span = document.getElementsByClassName("close")[0];
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
-let closeBtn = document.querySelector(".close-btn")
+let closeBtn = document.querySelector(".close-btn");
 closeBtn.onclick = function () {
-  modal.style.display="none"
-}
-
+  modal.style.display = "none";
+};
