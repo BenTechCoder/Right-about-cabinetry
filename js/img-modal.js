53f0e3842e2bc990dc5abf32b78ddac604048f76
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2021-11-22 17:04:42 -0500

    changing caption in img galleries

--- js/img-modal.js
+++ js/img-modal.js
@@ -4,7 +4,7 @@ let img = document.querySelector(".services-img");
 let captionText = document.querySelector(".caption");
 Array.from(document.querySelectorAll(".services-img")).forEach((item) => {
   item.onclick = function () {
-    modal.style.display = "block";
+    modal.style.display = "grid";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
    console.log(captionText)
